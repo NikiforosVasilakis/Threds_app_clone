fc27daeeaef9f6d4b04d6b7dc1985c5a0f5f67e5
NikiforosVasilakis <nikiforos.vasilakis@gmail.com>
2025-05-14 02:56:47 +0300

    Final commit

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -1,23 +1,25 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-let isConnected = false;
+let isConnected = false; // Variable to track the connection status
 
-export const connectToDB = async() => {
-    mongoose.set('strictQuery', true);
+export const connectToDB = async () => {
+  // Set strict query mode for Mongoose to prevent unknown field queries.
+  mongoose.set("strictQuery", true);
 
-    if(!process.env.MONGOBD_URL) return console.log('URL NOT FOUND');
-   
-    if(isConnected){
-        console.log('CONNECTED');
-        return;
-    } 
+  if (!process.env.MONGODB_URL) return console.log("Missing MongoDB URL");
 
-    try{
-        await mongoose.connect(process.env.MONGOBD_URL);
-        isConnected = true;
-        console.log("CONNECTED");
-    } catch (error){
-        console.log('URL NOT FOUND')
-    }
+  // If the connection is already established, return without creating a new connection.
+  if (isConnected) {
+    console.log("MongoDB connection already established");
+    return;
+  }
 
-}
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+
+    isConnected = true; // Set the connection status to true
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
